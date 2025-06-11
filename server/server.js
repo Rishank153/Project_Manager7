63cb874cd07b7bbf16dca9cd2437dc4702a0af76
Rishank153 <rishank842@gmail.com>
2025-06-11 09:03:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,11 +15,7 @@ const app = express();
 
 
 // Middleware
-app.use(cors({
-  origin: 'https://sparkly-kleicha-29c9f2.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
